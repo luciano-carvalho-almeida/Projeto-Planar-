d8abbf972c0b43b34209124c89ffd504f9e0ea32
luciano-carvalho-almeida <lucianoocarvalho216@gmail.com>
2026-05-26 19:48:16 -0300

    primeiro commit

---
+++ src/test/java/com/unifan/planar/PlanarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifan.planar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
